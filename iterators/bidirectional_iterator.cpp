67857b73bd75bbe38b1be2ca5a230b675a452834
jmontiel <jes.monti.503@gmail.com>
2025-08-05 22:32:10 -0600

    add: bidirectional iterator

---
+++ iterators/bidirectional_iterator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <list>
+
+int main() {
+  std::list<int> numbers = {1,2,3,4,5};
+
+  std::cout << "Traversing the list forwards:\n";
+  // Adaptador iterator
+  for(std::list<int>::iterator it = numbers.begin(); it != numbers.end(); ++it)
+  {
+    std::cout << *it << " ";
+  }
+  std::cout << "\n";
+
+  std::cout << "Traversing the list backwars:\n";
+  // Adaptador reverse iterator
+  for(std::list<int>::reverse_iterator rit = numbers.rbegin(); rit != numbers.rend(); ++rit)
+  {
+    std::cout << *rit << " ";
+  }
+  std::cout << "\n";
+
+  return 0;
+}
